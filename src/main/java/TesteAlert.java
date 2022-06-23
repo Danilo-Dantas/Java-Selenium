4c9fc656f77da7163a8c0526808ee77c4b739b73
Danilo <danilodantasofc@gmail.com>
2022-06-23 17:39:48 -0300

    criando interacao com alertPrompt

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -13,7 +13,7 @@ public class TesteAlert {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
+
 		driver.findElement(By.id("alert")).click();
 		Alert alert = driver.switchTo().alert();
 		String texto = alert.getText();
@@ -22,60 +22,69 @@ public class TesteAlert {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
 		driver.quit();
 	}
-	
+
 	@Test
-	public void deveInteragirComAlertConfirmaAceitando() throws InterruptedException {
+	public void deveInteragirComAlertConfirmAceitando() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C://driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
+
 		driver.findElement(By.id("confirm")).click();
 		Alert alertConfirm = driver.switchTo().alert();
 		alertConfirm.accept();
 		String texto = alertConfirm.getText();
 		alertConfirm.accept();
-		
+
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
-		
+
 		Thread.sleep(2000);
 		driver.quit();
 	}
-	
+
 	@Test
-	public void deveInteragirComAlertConfirmaNegando() throws InterruptedException {
+	public void deveInteragirComAlertConfirmNegando() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C://driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
+
 		driver.findElement(By.id("confirm")).click();
 		Alert alertConfirm = driver.switchTo().alert();
 		alertConfirm.dismiss();
 		String texto = alertConfirm.getText();
 		alertConfirm.accept();
-		
+
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+	@Test
+	public void deveInteragirComAlertPrompt() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C://driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("prompt")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Digite um numero", alert.getText());
+		alert.sendKeys("22");
+		Thread.sleep(1000);
+		alert.accept();
+		Assert.assertEquals("Era 22?", alert.getText());
+		Thread.sleep(1000);
+		alert.accept();
+		Assert.assertEquals(":D", alert.getText());
+		Thread.sleep(1000);
+		alert.accept();
 		
 		Thread.sleep(2000);
 		driver.quit();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
