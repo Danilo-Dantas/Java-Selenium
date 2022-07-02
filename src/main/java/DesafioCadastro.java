089914dc6e7847f9f057aef5e0b1461624d933e5
Danilo Dantas <danilodantasofc@gmail.com>
2022-07-02 18:31:06 -0300

    finalizando desafio cadastro

--- src/main/java/DesafioCadastro.java
+++ src/main/java/DesafioCadastro.java
@@ -1,7 +1,11 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class DesafioCadastro {
 
@@ -13,6 +17,17 @@ public class DesafioCadastro {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Danilo");
 		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Abreu");
 		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		combo.selectByIndex(4);
+		WebElement esportes = driver.findElement(By.id("elementosForm:esportes"));
+		Select comboEsportes = new Select(esportes);
+		comboEsportes.selectByIndex(0);
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Criar novas atualizações dessa páginas :P");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		WebElement res = driver.findElement(By.xpath("/html/body/center/div[2]/span"));
+		Assert.assertEquals("Cadastrado!", res.getText()); 
 		
 		Thread.sleep(2000);
 		driver.quit();
