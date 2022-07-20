850e2967e7ad7044b76aa07c5061fcbd64b033b5
Danilo <danilodantasofc@gmail.com>
2022-07-20 09:54:11 -0300

    teste espera explicita

--- src/main/java/TesteSincronismo.java
+++ src/main/java/TesteSincronismo.java
@@ -3,8 +3,12 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteSincronismo {
 	
@@ -38,5 +42,13 @@ public class TesteSincronismo {
 		dsl.clicarBotao("buttonDelay");
 		dsl.escrever("novoCampo", "Deu certo?");
 		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void deveUtilizarEsperaExplicita() throws InterruptedException {
+		dsl.clicarBotao("buttonDelay");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("novoCampo")));
+		dsl.escrever("novoCampo", "Deu certo?");
 	}
 }
